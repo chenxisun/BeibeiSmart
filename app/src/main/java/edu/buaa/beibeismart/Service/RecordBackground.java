fe39a017cbe454322c6567c6387f58759623bbbd
fan <fansluck@qq.com>
2018-05-05 17:40:18 +0800

    first

--- app/src/main/java/edu/buaa/beibeismart/Service/RecordBackground.java
+++ app/src/main/java/edu/buaa/beibeismart/Service/RecordBackground.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.service.carrier.CarrierMessagingService;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
