ccb92665d1156cc46841214b9b14381af435099f
GrayPlane123 <814264633@qq.com>
2018-05-21 22:14:26 +0800

    jjjj

---
+++ app/src/main/java/edu/buaa/beibeismart/Activity/Baike_2_Activity.java
@@ -0,0 +1,24 @@
+package edu.buaa.beibeismart.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.buaa.beibeismart.R;
+
+public class Baike_2_Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baike_2_);
+    }
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public void onVolleyFinish(int isSuccess, Object result) {
+
+    }
+}
---
+++ app/src/main/java/edu/buaa/beibeismart/Activity/Translation.java
@@ -0,0 +1,40 @@
+package edu.buaa.beibeismart.Activity;
+
+import java.util.Date;
+import java.util.List;
+
+public class Translation {
+    public List<data> content;
+    public static class data{
+        public int id;
+        private int verson;
+        private int deleted;
+        private Date createTime;
+        private Date updateTime;
+        public String name;
+        public String img1Path;
+        public String img2Path;
+        public String img3Path;
+        public String img4Path;
+        public String voicePath;
+    }
+    private boolean last;
+    private int totalElements;
+    private int totalPages;
+    private String sort;
+    private boolean first;
+    private int numberOfElements;
+    private int size;
+    private int number;
+
+    public void show(){
+        System.out.println("sdsdsdsd");
+        System.out.println(content.get(0).name);
+        System.out.println(content.get(0).img1Path);
+    }
+
+    public List<data> getContent(){
+        return content;
+    }
+
+}
